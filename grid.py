018743e0174c2d6c475115c6652f73d4426a7652
uides <145918566+uides1@users.noreply.github.com>
2024-03-26 05:52:26 +0700

    Update grid.py

--- grid.py
+++ grid.py
@@ -5,7 +5,6 @@ class GridPosition(enum.Enum):
    X = 1,
    O = 2
 
-
 class Grid:
     def __init__(self, rows, columns):
         self.rows = rows
@@ -23,3 +22,59 @@ class Grid:
         return self.columns
 
     def playMove(self, row, column, letter):
+        if self.columns <= column or column < 0:
+            return ValueError
+        elif self.rows <= row or row < 0 :
+            return ValueError
+        if letter == GridPosition.EMPTY:
+            return ValueError
+        if self.grid[row][column] == GridPosition.EMPTY:
+            self.grid[row][column] = letter
+            print('worked')
+            return row, column
+        return ValueError
+
+    def winner(self, wc, row, column, letter):  # wc = win condition
+        count = 0
+        for r in range(self.columns):
+            if self.grid[r][column] == letter:
+                count += 1
+            else:
+                count = 0
+            if count == wc:
+                return True
+        count = 0
+        for c in range(self.columns):
+            if self.grid[row][c] == letter:
+                count += 1
+            else:
+                count = 0
+            if count == wc:
+                return True
+        count = 0
+        for r in range(self.rows):
+            c = row + column - r
+            if 0 <= c < self.columns and self.grid[r][c] == letter:
+                count += 1
+            else:
+                count = 0
+            if count == wc:
+                return True
+        count = 0
+        for r in range(self.rows):
+            c = column - row + r
+            if 0 <= c < self.columns and self.grid[r][c] == letter:
+                count += 1
+            else:
+               count = 0
+            if count == wc:
+                return True
+        return False
+   
+    def availableMove(self):
+        available = []
+        for row in range(self.rows):
+            for column in range(self.columns):
+                if self.grid[row][column] == GridPosition.EMPTY:
+                    available.append([row, column])
+        return available
